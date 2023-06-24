269bf2287400b85221112bfcbd94baa24a5c3315
Pasith <pasiths@users.noreply.github.com>
2023-06-24 17:16:05 +0530

    .

--- MSSQL_USE/Melody Music-Theams_1/Melody Music-Theams_1/Loadding.Designer.cs
+++ MSSQL_USE/Melody Music-Theams_1/Melody Music-Theams_1/Loadding.Designer.cs
@@ -68,7 +68,7 @@ namespace Melody_Music_Theams_1
             this.pictureBox1.Location = new System.Drawing.Point(475, 256);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(485, 449);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
